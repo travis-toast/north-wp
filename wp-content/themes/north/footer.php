8646fa32a79703c69f13338d056cd592a7f6f62f
mark-toast <toast05@toast05-2.local>
2014-11-10 15:35:27 +1100

    Return splash page/ add analytics

--- wp-content/themes/north/footer.php
+++ wp-content/themes/north/footer.php
@@ -59,6 +59,22 @@
 	<script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/_assets/scripts/jquery.min.js"><\/script>')</script>
 
 	<script src="<?php echo get_template_directory_uri(); ?>/_assets/scripts/plugins.min.js"></script>
+
+	<script>
+
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-4805518-77']);
+		  _gaq.push(['_trackPageview']);
+		
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		
+	</script>
+
+
 </body>
 
 </html>
