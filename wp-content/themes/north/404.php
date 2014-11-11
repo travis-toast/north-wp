4f0cab9667f3d236d69412683bf2dd14cf75905a
mark-toast <toast05@toast05-2.local>
2014-11-11 13:56:22 +1100

    404 error page

--- wp-content/themes/north/404.php
+++ wp-content/themes/north/404.php
@@ -1,7 +1,25 @@
 <?php get_header(); ?>
 
-	<h2>Error 404 - Page Not Found</h2>
+			
 
-<?php get_sidebar(); ?>
+	<header class="pageheader fadeinfromside pushdown">
+		<h1 class="title">Error 404 - Page Not Found</h1>
+		<span class="subtitle"><?php the_field('subtitle'); ?></span>
+	</header>
+
+
+	<div class="wrapper">
+
+		<div class="row">
+		</div>
+		<div class="row">
+			<div class="plain-txt">
+			</div>
+		</div>
+	</div>
+
+
+
+
 
 <?php get_footer(); ?>
